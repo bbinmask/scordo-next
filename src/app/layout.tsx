7cbd5b8010bda53dbd63a45b3a335483ceb5405d
BB in Mask <bbinmask@gmail.com>
2025-09-02 11:08:35 +0530

    Cleaning code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,5 @@
 import type { Metadata } from "next";
-import { Urbanist, Cal_Sans, Poppins } from "next/font/google";
 import "./globals.css";
-
-const calSans = Cal_Sans({
-  subsets: ["latin"],
-  weight: ["400"],
-});
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-const urbanist = Urbanist({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin", "latin-ext"],
-  style: ["normal"],
-});
-export const metadata: Metadata = {
-  title: "Scordo",
-  description: "Only platform that manages your cricket teams and scores",
-};
 
 export default function RootLayout({
   children,
@@ -29,11 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="layout-background dark:bg-red-600">
-        <main
-          className={`min-h-screen w-full ${poppins.className} ${calSans.className} ${urbanist.className} antialiased`}
-        >
-          {children}
-        </main>
+        <main className={`min-h-screen w-full antialiased`}>{children}</main>
       </body>
     </html>
   );
