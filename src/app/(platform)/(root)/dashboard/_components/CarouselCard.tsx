aec75194842aba724b316cee5e16febfaaef78cb
BB in Mask <bbinmask@gmail.com>
2025-09-07 16:26:21 +0530

    Overflow is fixed

--- src/app/(platform)/(root)/dashboard/_components/CarouselCard.tsx
+++ src/app/(platform)/(root)/dashboard/_components/CarouselCard.tsx
@@ -8,7 +8,10 @@ import React from "react";
 export const LiveMatchCard = ({ className, match }: { className?: string; match: any }) => {
   return (
     <div className={cn("relative grid", className)}>
-      <Link href="#" className="text-foreground mb-2 w-fit font-[poppins] text-xs hover:underline">
+      <Link
+        href="#"
+        className="text-foreground mb-2 w-fit font-[poppins] text-[10px] hover:underline"
+      >
         Indian Premier League 2025
       </Link>
       <Link
@@ -16,28 +19,28 @@ export const LiveMatchCard = ({ className, match }: { className?: string; match:
         className="block"
       >
         <div className="mb-2 grid cursor-pointer grid-cols-1">
-          <h2 className="primary-heading font-[cal_sans] text-lg font-semibold tracking-widest">
+          <h2 className="primary-heading font-[cal_sans] text-base font-semibold tracking-widest">
             {shortenTeamName("Royal Challengers Bangaluru")}
             <span className="mx-2">vs</span>
             {shortenTeamName("Chennai Super Kings")}
           </h2>
         </div>
-        <p className="mb-1 font-[poppins] text-sm text-gray-700 dark:text-gray-300">
+        <p className="text-foreground mb-1 font-[poppins] text-xs">
           {shortenTeamName("Royal Challengers Bangaluru")}:{" "}
-          <span className="font-semibold">{"205/5 (20 overs)"}</span>
+          <span className="font-medium">{"205/5 (20 overs)"}</span>
         </p>
-        <p className="font-[poppins] text-sm text-gray-700 dark:text-gray-300">
+        <p className="text-foreground font-[poppins] text-xs">
           {shortenTeamName("Chennai Super Kings")}:{" "}
-          <span className="font-semibold">{"105/5 (15 overs)"}</span>
+          <span className="font-medium">{"105/5 (15 overs)"}</span>
         </p>
-        <p className="font-[urbanist] text-sm leading-10 font-semibold text-red-500">
+        <p className="text-xs leading-10 font-semibold text-red-500">
           {`${shortenTeamName("Chennai Super Kings")} needs 101 runs in 30 balls`}
         </p>
         <div className="absolute right-6 bottom-6">
           <ChevronRight className="h-5 w-5 text-gray-400 dark:text-gray-500" />
         </div>
       </Link>
-      <div className="absolute top-6 right-6">
+      <div className="absolute top-2 right-6">
         <span className="animate-pulse text-sm font-semibold text-red-500 dark:text-red-400">
           ● {"Live"}
         </span>
@@ -50,11 +53,11 @@ export const UpcomingMatchCard = ({ className, match }: { className?: string; ma
   return (
     <div className={cn("flex flex-row flex-nowrap", className)}>
       <div className="w-full">
-        <Link href="#" className="text-foreground mb-2 font-[poppins] text-xs hover:underline">
+        <Link href="#" className="text-foreground mb-2 font-[poppins] text-[10px] hover:underline">
           Indian Premier League 2025
         </Link>
         <div className="mb-2 grid cursor-pointer grid-cols-1">
-          <h2 className="primary-heading font-[cal_sans] text-lg font-bold tracking-widest">
+          <h2 className="primary-heading font-[cal_sans] text-base font-bold tracking-widest">
             {shortenTeamName("Royal Challengers Bangaluru")}
             <span className="mx-2">vs</span>
             {shortenTeamName("Chennai Super Kings")}
