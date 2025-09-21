b56c5a0cfaeee5f710e9b3606ff10129e5b4a654
BB in Mask <bbinmask@gmail.com>
2025-09-21 19:44:38 +0530

    Design homepage

--- src/app/(platform)/(root)/layout.tsx
+++ src/app/(platform)/(root)/layout.tsx
@@ -1,6 +1,8 @@
+import { Sidebar } from "@/components/ui/sidebar";
 import Bottombar from "./_components/Bottombar";
 import Navbar from "./_components/Navbar";
 import QueryProvider from "@/components/providers/QueryProvider";
+import { SidebarDemo } from "./_components/SidebarDemo";
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryProvider>
@@ -8,6 +10,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <Navbar />
       </nav>
       <div className="flex justify-center">
+        {/* <SidebarDemo></SidebarDemo> */}
         <div className="w-full max-w-[1200px] pb-[70px]">{children}</div>
       </div>
       <Bottombar />
