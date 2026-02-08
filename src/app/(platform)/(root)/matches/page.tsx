9963bb34552678178f4c35574bbe1afebf6dac35
BB in Mask <bbinmask@gmail.com>
2026-02-08 20:41:11 +0530

    match-request is working properly

--- src/app/(platform)/(root)/matches/page.tsx
+++ src/app/(platform)/(root)/matches/page.tsx
@@ -12,6 +12,7 @@ import Spinner from "@/components/Spinner";
 import { toast } from "sonner";
 import { useAction } from "@/hooks/useAction";
 import { MatchWithTeamAndOfficials } from "@/lib/types";
+import { acceptMatchRequest, declineMatchRequest } from "@/actions/match-actions";
 
 const MatchCard = ({ match }: { match: any }) => {
   if (!match) return null;
@@ -134,8 +135,6 @@ const EmptyState = ({ type = "managed" }) => (
 
 function MatchRequests() {
   const [inviteId, setInviteId] = useState<string | null>(null);
-  const isAccepting = false;
-  const isCanceling = false;
   const queryClient = useQueryClient();
 
   const { data: matchRequests, isLoading } = useQuery<MatchWithTeamAndOfficials[]>({
@@ -147,39 +146,39 @@ function MatchRequests() {
     },
   });
 
-  // const { execute: executeAccept, isLoading: isAccepting } = useAction(acceptTeamRequest, {
-  //   onSuccess(data) {
-  //     queryClient.invalidateQueries({ queryKey: ["team-invites"] });
-  //     toast.success("Accepted!");
-  //     setInviteId(null);
-  //   },
-  //   onError(error) {
-  //     toast.error(error);
-  //     setInviteId(null);
-  //   },
-  // });
+  const { execute: executeAccept, isLoading: isAccepting } = useAction(acceptMatchRequest, {
+    onSuccess(data) {
+      queryClient.invalidateQueries({ queryKey: ["match-requests"] });
+      toast.success("Accepted!");
+      setInviteId(null);
+    },
+    onError(error) {
+      toast.error(error);
+      setInviteId(null);
+    },
+  });
 
-  // const { execute: executeDecline, isLoading: isCanceling } = useAction(declineTeamRequest, {
-  //   onSuccess(data) {
-  //     queryClient.invalidateQueries({ queryKey: ["team-invites"] });
-  //     toast.success("Request Declined!");
-  //     setInviteId(null);
-  //   },
-  //   onError(error) {
-  //     toast.error(error);
-  //     setInviteId(null);
-  //   },
-  // });
+  const { execute: executeDecline, isLoading: isCanceling } = useAction(declineMatchRequest, {
+    onSuccess(data) {
+      queryClient.invalidateQueries({ queryKey: ["team-invites"] });
+      toast.success("Request Declined!");
+      setInviteId(null);
+    },
+    onError(error) {
+      toast.error(error);
+      setInviteId(null);
+    },
+  });
 
-  const handleAccept = (id: string, teamId: string, fromId: string) => {
+  const handleAccept = (id: string) => {
     setInviteId(id);
 
-    // executeAccept({ teamId, reqId: id, fromId });
+    executeAccept({ id });
   };
   const handleDecline = (id: string) => {
     setInviteId(id);
 
-    // executeDecline({ id, teamId });
+    executeDecline({ id });
   };
 
   return (
@@ -200,65 +199,67 @@ function MatchRequests() {
       </div>
 
       {/* Team Invitations */}
-      {isLoading ? (
-        <div className="center flex h-full w-full">
-          <Spinner />
-        </div>
-      ) : !matchRequests || matchRequests.length === 0 ? (
-        <div className="rounded-[2rem] border border-dashed border-slate-200 p-6 text-center dark:border-white/10">
-          <p className="text-[10px] font-normal tracking-widest text-slate-400 uppercase">
-            No Requests found!
-          </p>
-        </div>
-      ) : (
-        <ul className="hide_scrollbar mb-4 max-h-36 overflow-y-auto scroll-smooth rounded-xl">
-          {matchRequests.map((invite) => (
-            <li key={invite.id} className="rounded-lg px-3 py-2">
-              <div className="mb-2 flex items-center justify-between">
-                <div className="flex items-center space-x-2">
-                  <img
-                    src={invite?.teamA?.logo || "/team.svg"}
-                    alt={invite.teamA.name}
-                    className="h-8 w-8 rounded-full bg-white"
-                    onError={(e) =>
-                      (e.currentTarget.src = "https://placehold.co/100x100/CCCCCC/FFFFFF?text=T")
-                    }
-                  />
-                  <h3 className="font-[urbanist] font-bold text-slate-800 dark:text-slate-100">
-                    {invite.teamA.name}
-                  </h3>
+      <div className="max-h-36 overflow-x-hidden overflow-y-auto">
+        {isLoading ? (
+          <div className="center flex h-full w-full">
+            <Spinner />
+          </div>
+        ) : !matchRequests || matchRequests.length === 0 ? (
+          <div className="mt-8 rounded-[2rem] border border-dashed border-slate-200 p-6 text-center dark:border-white/10">
+            <p className="text-[10px] font-normal tracking-widest text-slate-400 uppercase">
+              No Requests found!
+            </p>
+          </div>
+        ) : (
+          <ul className="hide_scrollbar mb-4 overflow-y-auto scroll-smooth rounded-xl">
+            {matchRequests.map((req) => (
+              <li key={req.id} className="rounded-lg px-3 py-2">
+                <div className="mb-2 flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <img
+                      src={req?.teamA?.logo || "/team.svg"}
+                      alt={req.teamA.name}
+                      className="h-8 w-8 rounded-full bg-white"
+                      onError={(e) =>
+                        (e.currentTarget.src = "https://placehold.co/100x100/CCCCCC/FFFFFF?text=T")
+                      }
+                    />
+                    <h3 className="font-[urbanist] font-bold text-slate-800 dark:text-slate-100">
+                      {req.teamA.name}
+                    </h3>
+                  </div>
                 </div>
-              </div>
-              {
-                <div className="flex space-x-2 font-[poppins]">
-                  <button
-                    disabled={isAccepting || isCanceling}
-                    onClick={() => handleAccept("", "", "")}
-                    className="flex-1 rounded-xl bg-green-600 py-2 text-[10px] font-bold text-white uppercase transition-opacity hover:opacity-90"
-                  >
-                    {isAccepting && inviteId === invite.id ? (
-                      <Spinner className="mx-auto h-4" />
-                    ) : (
-                      "Accept"
-                    )}
-                  </button>
-                  <button
-                    onClick={() => handleDecline(invite.id)}
-                    disabled={isCanceling || isAccepting}
-                    className="center flex flex-1 rounded-xl bg-slate-200 py-2 text-[10px] font-bold text-slate-600 uppercase transition-colors hover:bg-slate-300 dark:bg-white/10 dark:text-slate-400"
-                  >
-                    {isCanceling && inviteId === invite.id ? (
-                      <Spinner className="mx-auto h-4" />
-                    ) : (
-                      <span className="text-red-600 dark:text-red-300">Decline</span>
-                    )}
-                  </button>
-                </div>
-              }
-            </li>
-          ))}
-        </ul>
-      )}
+                {
+                  <div className="flex space-x-2 font-[poppins]">
+                    <button
+                      disabled={isAccepting || isCanceling}
+                      onClick={() => handleAccept(req.id)}
+                      className="flex-1 rounded-xl bg-green-600 py-2 text-[10px] font-bold text-white uppercase transition-opacity hover:opacity-90"
+                    >
+                      {isAccepting && inviteId === req.id ? (
+                        <Spinner className="mx-auto h-4" />
+                      ) : (
+                        "Accept"
+                      )}
+                    </button>
+                    <button
+                      onClick={() => handleDecline(req.id)}
+                      disabled={isCanceling || isAccepting}
+                      className="center flex flex-1 rounded-xl bg-slate-200 py-2 text-[10px] font-bold text-slate-600 uppercase transition-colors hover:bg-slate-300 dark:bg-white/10 dark:text-slate-400"
+                    >
+                      {isCanceling && inviteId === req.id ? (
+                        <Spinner className="mx-auto h-4" />
+                      ) : (
+                        <span className="text-red-600 dark:text-red-300">Decline</span>
+                      )}
+                    </button>
+                  </div>
+                }
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
@@ -351,12 +352,11 @@ const MatchesPage = () => {
           </div>
 
           {/* Sidebar Column */}
-          <aside className="flex gap-2 space-y-8 px-4 lg:col-span-4 lg:block lg:px-0 lg:pr-2">
+          <aside className="gap-2 space-y-8 px-4 sm:flex lg:col-span-4 lg:block lg:px-0 lg:pr-2">
             {/* Create Match */}
             <CreateMatchCard />
 
             {/* Match Requests */}
-
             <MatchRequests />
           </aside>
         </div>
