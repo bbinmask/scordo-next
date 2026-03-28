b29f0cea3515d6a489d0c882f72772e0c75d0bce
BB in Mask <bbinmask@gmail.com>
2026-03-28 10:48:02 +0530

    Working on tournamentIdPage

--- src/app/(platform)/(root)/tournaments/layout.tsx
+++ src/app/(platform)/(root)/tournaments/layout.tsx
@@ -4,7 +4,7 @@ import { getMetadata } from "@/utils/helper/getMetadata";
 export const metadata = getMetadata("Tournaments | Scordo");
 
 const TournamentsLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="overflow-x-hidden px-4">{children}</div>;
+  return <div className="overflow-x-hidden">{children}</div>;
 };
 
 export default TournamentsLayout;
